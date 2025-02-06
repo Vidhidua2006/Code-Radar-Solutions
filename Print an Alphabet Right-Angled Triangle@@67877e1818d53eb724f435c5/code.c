1183244bb7b391e99d648b2b4dab50ea06713bae
Vidhidua2006 <vidhi1775.becse24@chitkara.edu.in>
2025-02-06 12:30:31 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -2,55 +2,15 @@
 
 int main() {
     int a;
-    scanf("%d", &a);  // Read an integer from input
-    
-    switch (a) {
-        case 1:
-            printf("A");
-            break;
-        case 2:
-            printf("A\n");
-            printf("A B");
-            break;
-        case 3:
-            printf("A\n");
-            printf("A B\n");
-            printf("A B C\n");
-            break;
-        case 4:
-            printf("A\n");
-            printf("A B\n");
-            printf("A B C\n");
-            printf("A B C D\n");
-            break;
-        case 5:
-            printf("A\n");
-            printf("A B\n");
-            printf("A B C\n");
-            printf("A B C D\n");
-            printf("A B C D E\n");
-            break;
-        case 6:
-            printf("A\n");
-            printf("A B\n");
-            printf("A B C\n");
-            printf("A B C D\n");
-            printf("A B C D E\n");
-            printf("A B C D E F\n");
-            break;
-        case 7:
-            printf("A\n");
-            printf("A B\n");
-            printf("A B C\n");
-            printf("A B C D\n");
-            printf("A B C D E\n");
-            printf("A B C D E F\n");
-            printf("A B C D E F G\n");
-            break;
-        
-        default:  
-            printf("Error");
-            break;
+    char c;
+    scanf("%d", &a);  
+    if(c<=A&&c>=Z){
+        for(int i=1;i<=a;i++){
+            for(int j=0;j<=i;j++){
+                printf("%c",j);
+            }
+            printf("\n");
+        }
     }
     
     return 0;
