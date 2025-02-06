5558a309250121f056c2686ac452d0f0ae64dacc
Vidhidua2006 <vidhi1775.becse24@chitkara.edu.in>
2025-02-06 12:22:41 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -2,47 +2,38 @@
 
 int main() {
     int a;
-    scanf("%d",&a);
-    switch(a){
-        case 1 :
-        {
+    scanf("%d", &a);  // Read an integer from input
+    
+    switch (a) {
+        case 1:
             printf("A");
             break;
-        }
         case 2:
-        {
             printf("A\n");
             printf("A B");
             break;
-        }
         case 3:
-        {
             printf("A\n");
             printf("A B\n");
             printf("A B C\n");
             break;
-        }
         case 4:
-        {
             printf("A\n");
             printf("A B\n");
             printf("A B C\n");
             printf("A B C D\n");
             break;
-        }
         case 5:
-        {
             printf("A\n");
             printf("A B\n");
             printf("A B C\n");
             printf("A B C D\n");
             printf("A B C D E\n");
             break;
-        }
-        default{
+        default:  // This is the correct syntax for the default case
             printf("Error");
             break;
-        }
     }
+    
     return 0;
 }
